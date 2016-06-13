f0bc59b65b05f6ba3b1d96664ea115837f7459b5
acreed829 <acreed829@gmail.com>
2016-06-13 23:20:49 +0300

    Created toolbar & navigation view

--- app/src/main/java/su/acreed/boomerang/MainActivity.java
+++ app/src/main/java/su/acreed/boomerang/MainActivity.java
@@ -1,19 +1,19 @@
 package su.acreed.boomerang;
 
 
-
-import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
